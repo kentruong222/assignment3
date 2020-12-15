05943710a4eefad77738f42b3bd744cae38aac5f
unknown <kentruong222u@gmaile.com>
2020-12-15 18:58:21 -0500

    initial commit

--- App.js
+++ App.js
@@ -1,21 +1,264 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { Ionicons, FontAwesome, Entypo } from '@expo/vector-icons';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import Header from './components/Header';
+import Score from './components/Score';
+import Card from './components/Card';
+
+import randomizer from './randomizer';
+
+export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.renderCards = this.renderCards.bind(this);
+    this.resetCards = this.resetCards.bind(this);
+   
+    let sources = {
+      'fontawesome': FontAwesome,
+      'entypo': Entypo,
+      'ionicons': Ionicons
+    };
+
+    let cards = [
+      {
+        src: 'fontawesome',
+        name: 'heart',
+        color: 'red'
+      },
+      {
+        src: 'entypo',
+        name: 'feather',
+        color: '#7d4b12'
+      },
+      {
+        src: 'entypo',
+        name: 'flashlight',
+        color: '#f7911f'
+      },
+      {
+        src: 'entypo',
+        name: 'flower',
+        color: '#37b24d'
+      },
+      {
+        src: 'entypo',
+        name: 'moon',
+        color: '#ffd43b'
+      },
+      {
+        src: 'entypo',
+        name: 'youtube',
+        color: '#FF0000'
+      },
+      {
+        src: 'entypo',
+        name: 'shop',
+        color: '#5f5f5f'
+      },
+      {
+        src: 'fontawesome',
+        name: 'github',
+        color: '#24292e'
+      },
+      {
+        src: 'fontawesome',
+        name: 'skype',
+        color: '#1686D9'
+      },
+      {
+        src: 'fontawesome',
+        name: 'send',
+        color: '#1c7cd6'
+      },
+      {
+        src: 'ionicons',
+        name: 'ios-magnet',
+        color: '#d61c1c'
+      },
+      {
+        src: 'ionicons',
+        name: 'logo-facebook',
+        color: '#3C5B9B'
+      }
+    ];
+
+    let clone = JSON.parse(JSON.stringify(cards)); // create a completely new array from the array of cards
+
+    this.cards = cards.concat(clone); // combine the original and the clone
+
+
+    this.cards.map((obj) => {
+      let id = Math.random().toString(36).substring(7);
+      obj.id = id;
+      obj.src = sources[obj.src];
+      obj.is_open = false;
+    });
+
+    this.cards = this.cards.shuffle(); 
+    this.state = {
+      current_selection: [],
+      selected_pairs: [],
+      score: 0,
+      cards: this.cards
+    }
+  
+  }
+
+  render() {
+    return (
+      //sets indivvidual card row,
+      <View style={styles.container}>
+        <Header />
+        <View style={styles.body}>
+          { 
+            this.renderRows.call(this) 
+          }
+        </View>
+        <Score score={this.state.score} />
+        <Button
+          onPress={this.resetCards}
+          title="Reset"
+          color="#008CFA" 
+        />
+      </View>
+    );
+  }
+  
+
+  resetCards() { //reset UI, change to default state, close cards, and shuffle to random positions
+    let cards = this.cards.map((obj) => {
+      obj.is_open = false;
+      return obj;
+    });
+
+    cards = cards.shuffle();
+
+    this.setState({
+      current_selection: [],
+      selected_pairs: [],
+      cards: cards,
+      score: 0
+    });
+  }
+
+
+  renderRows() {  //create array of items 5 each, render each row with getRowsContent, and then render cards on each iteration with map function
+   
+   
+    let contents = this.getRowContents(this.state.cards);
+    return contents.map((cards, index) => {
+      return (
+        <View key={index} style={styles.row}>
+          { this.renderCards(cards) }
+        </View>
+      );
+    });
+   
+  }
+
+
+  renderCards(cards) { //accepts array of cards using (card), passing each card object as props, clickcard() passed as props and cardID used to identify similar cards
+    return cards.map((card, index) => {
+      return (
+        <Card 
+          key={index} 
+          src={card.src} 
+          name={card.name} 
+          color={card.color} 
+          is_open={card.is_open}
+          clickCard={this.clickCard.bind(this, card.id)} 
+        />
+      );
+    });
+  }
+
+
+  clickCard(id) { //retrieve card info and check it to continue code
+    let selected_pairs = this.state.selected_pairs;
+    let current_selection = this.state.current_selection;
+    let score = this.state.score;
+
+    let index = this.state.cards.findIndex((card) => {
+      return card.id == id;
+    });
+
+    let cards = this.state.cards;
+    
+    if(cards[index].is_open == false && selected_pairs.indexOf(cards[index].name) === -1){
+
+      //open card to add it to array of selected card
+      cards[index].is_open = true;
+
+      current_selection.push({ 
+        index: index,
+        name: cards[index].name
+      });
+
+      if(current_selection.length == 2){ // check if 2 cards are the same with the same icon, it they're good then the card stays open, if incorrect, first card is closed then second card
+        if(current_selection[0].name == current_selection[1].name){
+          score += 1;
+          selected_pairs.push(cards[index].name);
+        }else{
+         
+          cards[current_selection[0].index].is_open = false;
+
+          setTimeout(() => {
+            cards[index].is_open = false;
+            this.setState({
+              cards: cards
+            });
+          }, 500);
+        }
+
+        current_selection = [];
+      }
+
+      this.setState({ //click option gains update/change to the app because of changes
+        score: score,
+        cards: cards,
+        current_selection: current_selection
+      });
+
+    }
+  
+  }
+
+
+  getRowContents(cards) { //creating an array of arrays with four items each using (card)
+    let contents_r = [];
+    let contents = [];
+    let count = 0;
+    cards.forEach((item) => {
+      count += 1;
+      contents.push(item);
+      if(count == 4){
+        contents_r.push(contents)
+        count = 0;
+        contents = [];
+      }
+    });
+
+    return contents_r;
+  }
+
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignSelf: 'stretch',
+    backgroundColor: '#8BD3FF'
   },
+  row: {
+    flex: 1,
+    flexDirection: 'row'
+  },
+  body: {
+    flex: 18,
+    justifyContent: 'space-between',
+    padding: 10,
+    marginTop: 10
+  }
 });
